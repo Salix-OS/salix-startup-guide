92ff8d145b006b4620b59391ee8d6a254cc7a88a
Cyrille Pontvieux <jrdATenialisDOTnet>
2010-07-16 00:43:51 +0000

    New version for 13.0.1 with new images and updated translations and ODT version.

--- compile.sh
+++ compile.sh
@@ -2,35 +2,33 @@
 cd $(dirname $0)
 
 langask=$1
-for po in `ls po/*.po`;do
+if [ "$2" = "pdf" ]; then
+  pdf=yes
+fi
+for po in `ls po/*.po` po/en.po;do
   lang=$(echo $po|sed 's|po/\(.*\)\.po|\1|')
-  if [[ -z "$langask" || (! -z "$langask" && "$langask" = "$lang") ]]; then
+  if [[ -z "$langask" || "$langask" = "all" || (! -z "$langask" && "$langask" = "$lang") ]]; then
     echo "Compiling `echo $po|sed "s|po/||"`"
     name=SalixStartupGuide-$lang
     cp -r odt odt-$lang
     cp -L images/$lang/* odt-$lang/Pictures/
-    find odt-$lang -name '.svn' -exec rm -f '{}' \; 2>/dev/null
+    find odt-$lang -name '.svn' -exec rm -rf '{}' \; 2>/dev/null
     (
       cd odt-$lang
+      rm -f ../base-$lang.odt
       zip -r ../base-$lang.odt * >/dev/null
     )
     rm -rf odt-$lang
-    echo "Merging new strings for locale “$lang”..."
-    igettext-build.py base-$lang.odt $po -o $name.odt 2>&1 | grep -v -A1 'DeprecationWarning'
-    rm base-$lang.odt
-    echo "Generating PDF locale “$lang”..."
-    odt2pdf $name.odt
+    if [ "$lang" != "en" ]; then
+      echo "Merging new strings for locale “$lang”..."
+      igettext-build.py base-$lang.odt $po -o $name.odt 2>&1
+      rm base-$lang.odt
+    else
+      mv base-$lang.odt $name.odt
+    fi
+    if [ ! -z "$pdf" ]; then
+      echo "Generating PDF locale “$lang”..."
+      odt2pdf -vvv $name.odt
+    fi
   fi
 done
-if [[ -z "$langask" || (! -z "$langask" && "$langask" = "en") ]]; then
-  cp -r odt odt-en
-  cp -L images/en/* odt-en/Pictures/
-  find odt-en -name '.svn' -exec rm -f '{}' \; 2>/dev/null
-  (
-    cd odt-en
-    zip -r ../SalixStartupGuide-en.odt * >/dev/null
-  )
-  rm -rf odt-en
-  echo "Generating PDF locale “en”..."
-  odt2pdf SalixStartupGuide-en.odt
-fi
