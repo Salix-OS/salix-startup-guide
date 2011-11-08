1578d5326e1d4ba556a049562e2e8f2bc74a24b5
Pierrick Le Brun <pierrickATsimplynuxDOTnet>
2011-11-08 19:39:52 +0000

    fixing trunk directory

--- fix_grddl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd $(dirname "$0")
-for f in $(grep -l 'xmlns:grddl' odt/*); do
-  sed -i 's_xmlns:grddl="http://www\.w3\.org/2003/g/data-view#"__; s_grddl:transformation="http://docs\.oasis-open\.org/office/1\.2/xslt/odf2rdf\.xsl"__' $f
-done
